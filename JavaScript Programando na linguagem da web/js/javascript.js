3ed216804654f7c2c34335ed95240ce2f87f9a40
Gustavo Silva <gustavohenriquesilva124@gmail.com>
2020-11-20 23:57:52 -0300

    Adicionando um Novo paciente a Lista com addEventListener CLICK

--- JavaScript Programando na linguagem da web/js/javascript.js
+++ JavaScript Programando na linguagem da web/js/javascript.js
@@ -55,7 +55,42 @@ function mostraMensagem(){
   
 botaoAdicionar = document.querySelector("#adicionar-paciente")
 
-botaoAdicionar.addEventListener("click",function(){
-    console.log("Cliquei no botão para adicionar um paciente")
+botaoAdicionar.addEventListener("click",function(event){
+  event.preventDefault();// remove o comportamento padrão do botão, que recarrega a página.
+  console.log("Esta chamando a função.")
+  var form = document.querySelector("#form-adiciona")
+  // var altura = form.querySelector("#altura").value
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  nomeTd.textContent = nome;
+
+  var pesoTd = document.createElement("td");
+  pesoTd.textContent = peso;
+
+  var alturaTd = document.createElement("td");
+  alturaTd.textContent = altura;
+
+  var gorduraTd = document.createElement("td");
+  gorduraTd.textContent = gordura;
+
+  var imcTd = document.createElement("td");
+ 
+
+  pacienteTr.appendChild(nomeTd)// Coloque como Filho
+  pacienteTr.appendChild(pesoTd)
+  pacienteTr.appendChild(alturaTd)
+  pacienteTr.appendChild(gorduraTd)
+
+  var tabela = document.querySelector("#tabela-pacientes")
+  tabela.appendChild(pacienteTr)
+
+
 })
 
